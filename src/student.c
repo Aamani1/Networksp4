768e784fe3af2d17c4091a1fb9191516fc832ae1
Amani Konduru <akonduru2@student.gsu.edu>
2019-04-08 12:33:41 -0400

    commit

--- src/student.c
+++ src/student.c
@@ -1,4 +1,3 @@
-
 /*
  * student.c
  * Multithreaded OS Simulation for CS 2200
@@ -10,7 +9,7 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <unistd.h>
 #include "os-sim.h"
 
 #pragma GCC diagnostic push
@@ -34,7 +33,15 @@ extern void wake_up(pcb_t *process);
  * for your use.
  */
 static pcb_t **current;
+
 static pthread_mutex_t current_mutex;
+static pthread_mutex_t queue_mutex;
+static pthread_cond_t  signal;
+static int timeSlice = -1; 
+static unsigned int cpu_count_temp;
+
+static pcb_t* head;
+static pcb_t* tail;
 
 enum {FIFO, ROUND_ROBIN, SRTF} algorithm = FIFO;
 
@@ -90,24 +97,20 @@ static pcb_t* pop() {
  * schedule() is your CPU scheduler.  It should perform the following tasks:
  *
  *   1. Select and remove a runnable process from your ready queue which 
- *	you will have to implement with a linked list or something of the sort.
+ *  you will have to implement with a linked list or something of the sort.
  *
  *   2. Set the process state to RUNNING
  *
- *   3. Set the currently running process using the current array
- *
- *   4. Call context_switch(), to tell the simulator which process to execute
+ *   3. Call context_switch(), to tell the simulator which process to execute
  *      next on the CPU.  If no process is runnable, call context_switch()
  *      with a pointer to NULL to select the idle process.
- *
- *	The current array (see above) is how you access the currently running process indexed by the cpu id. 
- *	See above for full description.
- *	context_switch() is prototyped in os-sim.h. Look there for more information 
- *	about it and its parameters.
+ *  The current array (see above) is how you access the currently running process indexed by the cpu id. 
+ *  See above for full description.
+ *  context_switch() is prototyped in os-sim.h. Look there for more information 
+ *  about it and its parameters.
  */
 static void schedule(unsigned int cpu_id)
 {
-    /* FIX ME */
     pcb_t* removeNode = pop();
     if ((removeNode != NULL)) {
         removeNode->state =  PROCESS_RUNNING;
@@ -128,26 +131,12 @@ static void schedule(unsigned int cpu_id)
  */
 extern void idle(unsigned int cpu_id)
 {
-    /* FIX ME */
-    // schedule(0);
     pthread_mutex_lock(&queue_mutex);
     while (head == NULL) {
         pthread_cond_wait(&signal, &queue_mutex); 
     }
     pthread_mutex_unlock(&queue_mutex);
     schedule(cpu_id);
-
-
-    /*
-     * REMOVE THE LINE BELOW AFTER IMPLEMENTING IDLE()
-     *
-     * idle() must block when the ready queue is empty, or else the CPU threads
-     * will spin in a loop.  Until a ready queue is implemented, we'll put the
-     * thread to sleep to keep it from consuming 100% of the CPU time.  Once
-     * you implement a proper idle() function using a condition variable,
-     * remove the call to mt_safe_usleep() below.
-     */
-    // mt_safe_usleep(1000000);
 }
 
 
@@ -162,7 +151,6 @@ extern void idle(unsigned int cpu_id)
  */
 extern void preempt(unsigned int cpu_id)
 {
-    /* FIX ME */
     pthread_mutex_lock(&current_mutex);
     push(current[cpu_id]);
     current[cpu_id]->state = PROCESS_READY;
@@ -180,7 +168,6 @@ extern void preempt(unsigned int cpu_id)
  */
 extern void yield(unsigned int cpu_id)
 {
-    /* FIX ME */
     pthread_mutex_lock(&current_mutex);
     current[cpu_id]->state = PROCESS_WAITING;
     pthread_mutex_unlock(&current_mutex);
@@ -195,7 +182,6 @@ extern void yield(unsigned int cpu_id)
  */
 extern void terminate(unsigned int cpu_id)
 {
-    /* FIX ME */
     pthread_mutex_lock(&current_mutex);
     current[cpu_id]->state = PROCESS_TERMINATED;
     pthread_mutex_unlock(&current_mutex);
@@ -209,21 +195,17 @@ extern void terminate(unsigned int cpu_id)
  *
  *   1. Mark the process as READY, and insert it into the ready queue.
  *
- *   2. If the scheduling algorithm is Priority, wake_up() may need
- *      to preempt the CPU with the lowest priority to allow it to
+ *   2. If the scheduling algorithm is SRTF, wake_up() may need
+ *      to preempt the CPU with the highest remaining time left to allow it to
  *      execute the process which just woke up.  However, if any CPU is
  *      currently running idle, or all of the CPUs are running processes
- *      with higher priority than the one which just woke up, wake_up()
+ *      with a lower remaining time left than the one which just woke up, wake_up()
  *      should not preempt any CPUs.
- *	To preempt a process, use force_preempt(). Look in os-sim.h for
- * 	its prototype and the parameters it takes in.
- *
- *  NOTE: A high priority corresponds to a low number.
- *  i.e. 0 is the highest possible priority.
+ *  To preempt a process, use force_preempt(). Look in os-sim.h for 
+ *  its prototype and the parameters it takes in.
  */
 extern void wake_up(pcb_t *process)
 {
-    /* FIX ME */
     process->state = PROCESS_READY;
     push(process);
     if (algorithm == SRTF) {
@@ -250,31 +232,25 @@ extern void wake_up(pcb_t *process)
 
 /*
  * main() simply parses command line arguments, then calls start_simulator().
- * You will need to modify it to support the -r and -p command-line parameters.
+ * You will need to modify it to support the -r and -s command-line parameters.
  */
 int main(int argc, char *argv[])
 {
     unsigned int cpu_count;
 
-    /*
-     * Check here if the number of arguments provided is valid.
-     * You will need to modify this when you add more arguments.
-     */
-    if (argc != 2)
+    /* Parse command-line arguments */
+    if (argc < 2)
     {
         fprintf(stderr, "CS 2200 Project 4 -- Multithreaded OS Simulator\n"
-            "Usage: ./os-sim <# CPUs> [ -r <time slice> | -p ]\n"
+            "Usage: ./os-sim <# CPUs> [ -r <time slice> | -s ]\n"
             "    Default : FIFO Scheduler\n"
             "         -r : Round-Robin Scheduler\n"
-            "         -p : Priority Scheduler\n\n");
+            "         -s : Shortest Remaining Time First Scheduler\n\n");
         return -1;
     }
-
-    /* Parse the command line arguments */
     cpu_count = strtoul(argv[1], NULL, 0);
     cpu_count_temp = cpu_count;
 
-    /* FIX ME - Add support for -r and -p parameters*/
     int option = getopt(argc, argv, "rs");
     switch(option) {
         case 'r':
@@ -287,7 +263,8 @@ int main(int argc, char *argv[])
             break;
         default :
             algorithm = FIFO;  
-            timeSlice  = -1; 
+            timeSlice  = -1;   
+    }
 
     /* Allocate the current[] array and its mutex */
     current = malloc(sizeof(pcb_t*) * cpu_count);
@@ -295,6 +272,7 @@ int main(int argc, char *argv[])
     pthread_mutex_init(&current_mutex, NULL);
     pthread_mutex_init(&queue_mutex, NULL);
     pthread_cond_init(&signal, NULL);
+
     /* Start the simulator in the library */
     start_simulator(cpu_count);
     return 0;
@@ -302,3 +280,4 @@ int main(int argc, char *argv[])
 
 
 #pragma GCC diagnostic pop
+
